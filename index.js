9d0878035546f5ce99783a2fc030402cbdda8c8c
omsohom01 <omsohom01@gmail.com>
2024-11-28 23:50:17 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Portfolio from "./Portfolio"; // This imports your main React component
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Portfolio />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
